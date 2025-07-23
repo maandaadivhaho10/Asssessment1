d845f4d2cdc2c2b306ff47d87a8b5aec9d8d0432
adivhaho <maandaadivhaho74@gmail.com>
2025-07-23 15:05:59 -0700

    added responsive

--- src/homePage/Header.jsx
+++ src/homePage/Header.jsx
@@ -1,13 +1,12 @@
-
 import bg from "../assets/imageass1.jpg";
 import headpic from "../assets/adivhaho.png";
 import { FiShoppingCart } from 'react-icons/fi';
 import "../App.css";
 export default function Header() {
   return (
-     <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
       {/* Logo */}
-      
+       <header> 
       <div className="top-bar">
      <div className="logopic">
 
@@ -18,30 +17,31 @@ export default function Header() {
     />
   </div>
   <div className="nav" >
-  <nav >
+  <nav style={{ display: 'flex', alignItems: 'center', gap: '15px' }}>
     <a href="#shop">SHOP</a>
     <a href="#about">ABOUT</a>
-  </nav>
-  </div>
-
-  <div className="cart-container">
-      <FiShoppingCart size={24} color="#fff" />
+    <div className="cart-container">
+      <div className="cart"><FiShoppingCart size={24} color="#fff" /></div>
       <div className="cart-badge">0</div>
     </div>
+  </nav>
+  </div>
   
 </div>
 
+</header>
 
 
   {/* Centered Content */}
   <div className="content-at-center">
     <div className="stick white">
-      <h1 className="about">for colourful steps in life</h1>
+      <h1 className="about">for colourful <br />   steps in life</h1>
     </div>
     <div className="mybutton">
       <button>visit our stores</button>
     </div>
   </div>
 </div>
+ 
   );
 }
